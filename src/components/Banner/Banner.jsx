9595ca44e5983fc3838db15e0f7f32bff078be9e
ALNoman78 <abdullaalno078@gmail.com>
2024-12-23 16:07:15 +0600

    test - 1 ( passed )

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,11 +4,11 @@ import bannerImage from '../../assets/Book 1.png'
 
 const Banner = props => {
     return (
-        <div className="hero bg-[rgba(19,19,19,0.05)] md:p-20 p-12 rounded-xl mt-8">
+        <div className="hero bg-[rgba(19,19,19,0.05)] md:p-20 rounded-xl mt-8">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src={bannerImage}
-                    className="max-w-sm " />
+                    className="" />
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
                     <p className="py-6">
