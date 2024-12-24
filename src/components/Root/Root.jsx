e972b2f8e9ed701687675381a886f0f0ec134d90
ALNoman78 <abdullaalno078@gmail.com>
2024-12-24 18:21:35 +0600

    spninner added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Navbar from '../Navbar/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import { TailSpin } from 'react-loader-spinner'
 
-const Root = props => {
+const Root = () => {
+    const navigation = useNavigation()
     return (
         <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ? <TailSpin
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="tail-spin-loading"
+                    radius="1"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                /> :
+                    <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     )
